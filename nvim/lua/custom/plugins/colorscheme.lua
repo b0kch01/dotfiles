f7650f899e0f887bc8ca153198523ad75e3372bf
Nathan Choi <nath49ers@gmail.com>
2025-10-23 09:44:45 -0700

    nvim changes

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,4 +1,20 @@
 return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd [[colorscheme catppuccin]]
+    end,
+    opts = {
+      flavour = 'mocha',
+      transparent_background = false,
+      integerations = {
+        nvimtree = true,
+      },
+    },
+  },
   -- {
   --   'folke/tokyonight.nvim',
   --   priority = 1000,
@@ -11,24 +27,63 @@ return {
   --     vim.cmd [[colorscheme tokyonight]]
   --   end,
   -- },
-  {
-    'killitar/obscure.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-    },
-    config = function(_, opts)
-      require('obscure').setup(opts)
-      vim.cmd [[colorscheme obscure]]
-    end,
-  },
-  --
+  -- {
+  --   'killitar/obscure.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = false,
+  --   },
+  --   config = function(_, opts)
+  --     require('obscure').setup(opts)
+  --     vim.cmd [[colorscheme obscure]]
+  --   end,
+  -- },
+
   -- {
   --   'arzg/vim-colors-xcode',
   --   priority = 1000,
   --   init = function()
   --     vim.cmd.colorscheme 'xcodedarkhc'
+  --   end,
+  -- },
+  -- {
+  --   'V4N1LLA-1CE/xcodedark.nvim',
+  --   enabled = false,
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('xcodedark').setup {
+  --       transparent = false,
+  --
+  --       integrations = {
+  --         telescope = false,
+  --         nvim_tree = true,
+  --         gitsigns = false,
+  --         bufferline = true,
+  --         incline = false,
+  --         lazygit = false,
+  --         which_key = true,
+  --         notify = true,
+  --       },
+  --
+  --       -- Font weight customization
+  --       styles = {
+  --         comments = { italic = true },
+  --         keywords = { bold = true },
+  --         functions = {},
+  --         variables = {},
+  --         strings = {},
+  --         booleans = { bold = true },
+  --         types = {},
+  --         constants = {},
+  --         operators = {},
+  --         punctuation = {},
+  --       },
+  --
+  --       terminal_colors = true,
+  --     }
+  --     vim.cmd.colorscheme 'xcodedark'
   --   end,
   -- },
 }
