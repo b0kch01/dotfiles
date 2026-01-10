2defbb70c3d0ffe047137d07c0090e76f2f03972
Nathan Choi <nath49ers@gmail.com>
2026-01-10 00:41:00 -0800

    color

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
   {
+    'wongmjane/nerv-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd [[colorscheme nerv]]
+    end,
+  },
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
