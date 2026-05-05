0c853c1ef53971a0addf5601a7d09df8d14fa24a
Nathan Choi <nath49ers@gmail.com>
2026-05-05 11:24:18 -0700

    tree, cursor

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,30 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  lazy = true,
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    default_component_configs = {
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '󱧋',
+      },
+    },
+    filesystem = {
+      window = {
+        mappings = {
+          ['<leader>e'] = 'close_window',
+        },
+      },
+    },
+  },
+}
